0a9bfe0d5718132d8e3fac241eb9e9f55f1daa3d
penglingwei <penglingwei@zju.edu.cn>
2024-05-23 22:22:13 +0800

    lesson10:something went wrong

--- users/urls.py
+++ users/urls.py
@@ -6,4 +6,6 @@ app_name = 'users'
 urlpatterns = [
     path('', views.userpage, name="user"),
     path('register', views.registerpage, name="register"),
+    path('login', views.loginpage, name="login"),
+    path('logout', views.logoutpage, name="logout"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.http import HttpResponse
 
 # Create your views here.
@@ -7,11 +8,26 @@ from django.http import HttpResponse
 def userpage(request):
     return HttpResponse("This is User page.")
 
+def loginpage(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect("posts:list")
+    else: 
+        form = AuthenticationForm()
+    return render(request, 'users/login.html', {"form": form})
+
+def logoutpage(request):
+    if request.method == "POST":
+        logout(request)
+        return redirect("users:login")
+        
 def registerpage(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            login(request, form.save())
             return redirect("posts:list")
     else:
         form = UserCreationForm()
